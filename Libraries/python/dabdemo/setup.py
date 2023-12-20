935e334bcd8bc7f30f5da5b730d61d677d285bdd
prakashkancham <prakashk.kancham@gmail.com>
2023-12-20 15:26:17 +0530

    Updated

--- Libraries/python/dabdemo/setup.py
+++ Libraries/python/dabdemo/setup.py
@@ -5,11 +5,11 @@ import dabdemo
 
 setup(
   name = "dabdemo",
-  version = dabdemo.__0.1__,
-  author = dabdemo.__author__,
-  url = "https://<my-url>",
-  author_email = "<my-author-name>@<my-organization>",
-  description = "<my-package-description>",
+  version = 0.1.0,
+  author = Prakash,
+  url = "https://github.com/prakashkancham/databricks.git",
+  author_email = "prakash@123.com",
+  description = "Utilities for Databricks",
   packages = find_packages(include = ["dabdemo"]),
   entry_points={"group_1": "run=dabdemo.__main__:main"},
   install_requires = ["setuptools"]
