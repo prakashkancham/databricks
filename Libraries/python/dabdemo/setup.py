8e5b78c1024b22a1a0bc1032e67cdba93ce82974
prakashkancham <prakashk.kancham@gmail.com>
2023-12-21 18:03:22 +0530

    Updated

--- Libraries/python/dabdemo/setup.py
+++ Libraries/python/dabdemo/setup.py
@@ -5,7 +5,7 @@ import dabdemo
 
 setup(
   name = "dabdemo",
-  version = "dabdemo-0.0.1",
+  version = "0.0.1",
   author = "dabdemoauthor",
   url = "https://github.com/prakashkancham/databricks.git",
   author_email = "<my-author-name>@<my-organization>",
